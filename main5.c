9bdf456f246447343372d9e419756a859ec8331d
im-zero57 <leman000@sookmyung.ac.kr>
2019-09-19 16:07:38 +0900

    03_variable #1,#2,#3

--- main5.c
+++ main5.c
@@ -5,26 +5,35 @@
 
 int main()
 {
-	printf("Hello Sookmyung EE!¡¬n");
+	printf("Hello Sookmyung EE!\n");
 
 	int x;
-	
-	printf("bytes of int : %d¡¬n",sizeof(x) );
-
 	short y;
-	
-	printf("bytes of short : %d¡¬n",sizeof(y) );
-
 	char a;
-	
-	printf("bytes of char : %d¡¬n",sizeof(a) );
-
 	float b;
+	double c;	
 	
-	printf("bytes of float : %d¡¬n",sizeof(b) );
-
-	double c;
+		
+	printf("bytes of int : %d\n",sizeof(x) );
 	
-	printf("bytes of double : %d¡¬n",sizeof(c) );
+    printf("bytes of short : %d\n",sizeof(y) );
+    
+	printf("bytes of char : %d\n",sizeof(a) );
+    
+	printf("bytes of float : %d\n",sizeof(b) );
+    
+	printf("bytes of double : %d\n",sizeof(c) );
+	
+	int input_int;
+	float input_float;
+	
+	printf("enter integer : ");
+	scanf("%d", &input_int);
+	
+	printf("enter float : ");
+	scanf("%f", &input_float);
+	
+	printf("integer : %d, float : %f\n", input_int, input_float);
+	
 	return 0;
 }
