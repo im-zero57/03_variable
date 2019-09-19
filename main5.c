c40504ea017f9303083e42ac7b09db20aefb12b1
im-zero57 <leman000@sookmyung.ac.kr>
2019-09-19 15:29:05 +0900

    실습 1번과 실습2번
    실습 1번과 실습 2번 파일입니다

---
+++ main5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+	printf("Hello Sookmyung EE!¡¬n");
+
+	int x;
+	
+	printf("bytes of int : %d¡¬n",sizeof(x) );
+
+	short y;
+	
+	printf("bytes of short : %d¡¬n",sizeof(y) );
+
+	char a;
+	
+	printf("bytes of char : %d¡¬n",sizeof(a) );
+
+	float b;
+	
+	printf("bytes of float : %d¡¬n",sizeof(b) );
+
+	double c;
+	
+	printf("bytes of double : %d¡¬n",sizeof(c) );
+	return 0;
+}
